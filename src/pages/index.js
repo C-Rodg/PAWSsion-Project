dc31fa171bec6d0a39b870173541d608441881b9
C-Rodg <C-Rodg@users.noreply.github.com>
2019-01-25 21:56:44 -0800

    Completed initial styling of home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,17 @@
 // Libraries
 import React from 'react'
 import styled from 'styled-components'
+import { Link } from 'gatsby'
 
 // Components
 import PageLayout from '../components/pageLayout'
 import SEO from '../components/seo'
 import HeroHeader from '../components/heroHeader'
+
+// Images
+import PicDogTrio from '../images/dogs_trio.png'
+import PicDogSleeping from '../images/dogs_sleeping2.png'
+import PicDogCloseUp from '../images/dogs_looking.png'
 
 // Styles
 import PaddingWrapper from '../styles/paddingWrapper'
@@ -40,9 +46,45 @@ const Card = styled.article`
   }
 `
 
+const SubText = styled.div`
+  margin-bottom: 4px;
+  color: #738a94;
+  font-size: 0.8rem;
+  font-weight: 500;
+  text-transform: uppercase;
+  letter-spacing: 0.5px;
+`
+
+const TitleText = styled.div`
+  font-size: 1.4rem;
+  font-weight: 700;
+  margin-bottom: 12px;
+`
+
 const LeadCard = styled(Card)`
   flex-direction: row;
   flex: 1 1 100%;
+
+  .side-image {
+    width: 50%;
+    padding-right: 20px;
+  }
+
+  .side-right {
+    width: 50%;
+    padding: 30px 40px 30px 20px;
+  }
+`
+
+const DogImage = styled.img`
+  height: 100%;
+  object-fit: cover;
+  object-position: center center;
+  width: 100%;
+`
+
+const DogContentWrapper = styled.div`
+  padding: 0 30px 30px 30px;
 `
 
 const IndexPage = () => (
@@ -52,9 +94,57 @@ const IndexPage = () => (
     <PaddingWrapper background="#f5f8fb">
       <GridWrap>
         <StyledMainWrapper>
-          <LeadCard>Top Box</LeadCard>
-          <Card>bottom left</Card>
-          <Card>bottom right</Card>
+          <LeadCard>
+            <Link to="/about" className="side-image">
+              <DogImage src={PicDogTrio} alt="trio of rescue dogs" />
+            </Link>
+            <div className="side-right">
+              <Link to="/about">
+                <SubText>About Us</SubText>
+                <TitleText>Come Learn About Our Story</TitleText>
+
+                <div>
+                  **TODO** Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard dummy text ever since the 1500s, when an unknown
+                  printer took a galley of type and scrambled it to make a type
+                  specimen book. It has survived not only five centuries.
+                </div>
+              </Link>
+            </div>
+          </LeadCard>
+          <Card>
+            <Link to="/gallery">
+              <DogImage src={PicDogCloseUp} alt="dog close up" />
+            </Link>
+            <DogContentWrapper>
+              <Link to="/gallery">
+                <SubText>Gallery</SubText>
+                <TitleText>See Some of Our Rescues</TitleText>
+                <div>
+                  **TODO** Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard dummy text ever since the 1500s.
+                </div>
+              </Link>
+            </DogContentWrapper>
+          </Card>
+          <Card>
+            <Link to="/contact">
+              <DogImage src={PicDogSleeping} alt="dog resting" />
+            </Link>
+            <DogContentWrapper>
+              <Link to="/contact">
+                <SubText>Contact</SubText>
+                <TitleText>Get in Touch With Us</TitleText>
+                <div>
+                  **TODO** Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard dummy text ever since the 1500s.
+                </div>
+              </Link>
+            </DogContentWrapper>
+          </Card>
         </StyledMainWrapper>
       </GridWrap>
     </PaddingWrapper>
