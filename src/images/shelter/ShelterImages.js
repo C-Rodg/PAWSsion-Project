be2092ab087f11dbfa4c782e10384a42e008b78a
C-Rodg <C-Rodg@users.noreply.github.com>
2019-01-27 15:17:27 -0800

    Added shelter images, styled gallery

---
+++ src/images/shelter/ShelterImages.js
@@ -0,0 +1,53 @@
+import Image01 from './shelter_01.jpg'
+import Image02 from './shelter_02.jpg'
+import Image03 from './shelter_03.jpg'
+import Image04 from './shelter_04.jpg'
+import Image05 from './shelter_05.jpg'
+import Image06 from './shelter_06.jpg'
+import Image07 from './shelter_07.jpg'
+import Image08 from './shelter_08.jpg'
+import Image09 from './shelter_09.jpg'
+import Image10 from './shelter_10.jpg'
+import Image11 from './shelter_11.jpg'
+import Image12 from './shelter_12.jpg'
+import Image13 from './shelter_13.jpg'
+import Image14 from './shelter_14.jpg'
+import Image15 from './shelter_15.jpg'
+import Image16 from './shelter_16.jpg'
+import Image17 from './shelter_17.jpg'
+import Image18 from './shelter_18.jpg'
+import Image19 from './shelter_19.jpg'
+import Image20 from './shelter_20.jpg'
+import Image21 from './shelter_21.jpg'
+import Image22 from './shelter_22.jpg'
+import Image23 from './shelter_23.jpg'
+import Image24 from './shelter_24.jpg'
+
+const ShelterImages = [
+  { src: Image01, style: '', text: 'Dogs enjoying shelter' },
+  { src: Image02, style: '', text: 'A new rescue dog' },
+  { src: Image03, style: '', text: 'Rescue puppies in basket' },
+  { src: Image04, style: '', text: 'A cute puppy' },
+  { src: Image05, style: '', text: 'A volunteer hanging out with a rescue' },
+  { src: Image06, style: '', text: 'Showing the new rescues love' },
+  { src: Image07, style: '', text: 'Our dogs love attention!' },
+  { src: Image08, style: '', text: 'Transporting the puppies' },
+  { src: Image09, style: '', text: 'Such a cute dog face' },
+  { src: Image10, style: '', text: 'Cute dog looking' },
+  { src: Image11, style: '', text: 'New puppies!' },
+  { src: Image12, style: '', text: 'Distemper Rehabilitation Center' },
+  { src: Image13, style: '', text: 'Such photogenic dogs' },
+  { src: Image14, style: '', text: 'Dog in a shirt' },
+  { src: Image15, style: '', text: 'Our workers that make this possible' },
+  { src: Image16, style: '', text: 'Dogs enjoying their new home' },
+  { src: Image17, style: '', text: 'Dogs just need love' },
+  { src: Image18, style: '', text: 'Dog exploring the outdoors' },
+  { src: Image19, style: '', text: 'What a cute face!' },
+  { src: Image20, style: '', text: 'Petting a new rescue' },
+  { src: Image21, style: '', text: 'Dog smiling' },
+  { src: Image22, style: '', text: 'Before and after resuce' },
+  { src: Image23, style: '', text: 'What a cute puppy' },
+  { src: Image24, style: '', text: 'Malou showing dogs love' },
+]
+
+export default ShelterImages
--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -1,10 +1,14 @@
 // Libraries
-import React from 'react'
+import React, { Component } from 'react'
+import styled from 'styled-components'
 
 // Components
 import PageLayout from '../components/pageLayout'
 import SEO from '../components/seo'
 import NavBar from '../components/navBar'
+
+// Images
+import ShelterImages from '../images/shelter/ShelterImages'
 
 // Styles
 import PageTitle from '../styles/pageTitle'
@@ -12,39 +16,91 @@ import PaddingWrapper from '../styles/paddingWrapper'
 import GridWrap from '../styles/gridWrap'
 import PageParagraph from '../styles/pageParagraph'
 
-const Gallery = () => (
-  <PageLayout>
-    <SEO title="Gallery" />
-    <NavBar />
-    <PageTitle>
-      <h1>Gallery</h1>
-    </PageTitle>
-    <PaddingWrapper>
-      <GridWrap>
-        <PageParagraph>
-          Happy dogs make us happy. Please take a look at some of our past
-          rescues and our amazing volunteers that work to make this happen. To
-          stay up-to-date with our latest pups, please like our{' '}
-          <a
-            href="https://www.facebook.com/PAWSsionProject/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Facebook
-          </a>{' '}
-          page or give us a follow on{' '}
-          <a
-            href="https://www.instagram.com/pawssionproject/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Instagram
-          </a>
-          .
-        </PageParagraph>
-      </GridWrap>
-    </PaddingWrapper>
-  </PageLayout>
-)
+const ImageWrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, 320px);
+  grid-gap: 15px;
+  justify-content: center;
+  margin-bottom: 60px;
+  figure {
+    margin-bottom: 0;
+    width: 320px;
+    height: 240px;
+    cursor: pointer;
+    overflow: hidden;
+
+    &:hover {
+      img {
+        transform: scale3d(1.07, 1.07, 1);
+      }
+    }
+  }
+
+  img {
+    backface-visibility: hidden;
+    transform: scale3d(1, 1, 1);
+    transition: transform 1.8s ease;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+`
+
+class Gallery extends Component {
+  handleOpenImageModal = idx => () => {
+    console.log('Opening: ' + idx)
+  }
+
+  // Render image grid
+  renderImages() {
+    return ShelterImages.map((img, idx) => {
+      return (
+        <figure key={idx} onClick={this.handleOpenImageModal(idx)}>
+          <img src={img.src} alt={img.text} />
+        </figure>
+      )
+    })
+  }
+
+  render() {
+    return (
+      <PageLayout>
+        <SEO title="Gallery" />
+        <NavBar />
+        <PageTitle>
+          <h1>Gallery</h1>
+        </PageTitle>
+        <PaddingWrapper>
+          <GridWrap>
+            <PageParagraph>
+              Happy dogs make us happy. Please take a look at some of our past
+              rescues and our amazing volunteers that work to make this happen.
+              To stay up-to-date with our latest pups, please like our{' '}
+              <a
+                href="https://www.facebook.com/PAWSsionProject/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Facebook
+              </a>{' '}
+              page or give us a follow on{' '}
+              <a
+                href="https://www.instagram.com/pawssionproject/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Instagram
+              </a>
+              .
+            </PageParagraph>
+            <ImageWrapper className="shelter-wrapper">
+              {this.renderImages()}
+            </ImageWrapper>
+          </GridWrap>
+        </PaddingWrapper>
+      </PageLayout>
+    )
+  }
+}
 
 export default Gallery
